ed93a22ee373afd322be1d276b1c7c2af9814947
Lucas Ricart <lucas.ricart@laplateforme.io>
2025-03-24 14:17:34 +0100

    final
    may miss comment

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:admin@localhost/finance'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:pipes606&@localhost/finance'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'admin'
+    SECRET_KEY = 'pipes606&'
